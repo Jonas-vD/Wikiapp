df932f9de24977652749c21cb064a02ba35fe733
Jonas van Doorn <jonasvandoorn@gmail.com>
2021-12-16 23:39:19 +0100

    chart update

--- src/js/index.js
+++ src/js/index.js
@@ -24,20 +24,20 @@ let myChart = new Chart(chartRef, {
         label: "Amount of views a day",
         data: [],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235)",
+          "rgba(255, 206, 86)",
+          "rgba(75, 192, 192)",
+          "rgba(153, 102, 255)",
+          "rgba(255, 159, 64)",
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235)",
+          "rgba(255, 206, 86)",
+          "rgba(75, 192, 192)",
+          "rgba(153, 102, 255)",
+          "rgba(255, 159, 64)",
         ],
         borderWidth: 1,
       },
@@ -56,10 +56,17 @@ let myChart = new Chart(chartRef, {
 });
 
 const render = () => {
+  myChart.data.labels = [];
   const timestamps = store.getState().data.map((obj) => obj.timestamp);
   const amountViews = store.getState().data.map((obj) => obj.views);
-  myChart.data.labels.push(...timestamps);
-  myChart.data.datasets[0].data.push(...amountViews);
+  //   myChart.data.labels.push(...timestamps);
+  myChart.data.labels.splice(0, myChart.data.labels.length, ...timestamps);
+
+  myChart.data.datasets[0].data.splice(
+    0,
+    myChart.data.datasets[0].data.length,
+    ...amountViews
+  );
   myChart.update();
 };
 
