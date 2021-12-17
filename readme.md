5489b1a6358e58e9de6a56d42fccf621d88d41ee
Jonas van Doorn <jonasvandoorn@gmail.com>
2021-12-17 23:35:01 +0100

    responsive

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Wikiviews is a webapp where you can search how many views a page on wikipedia has.
 Search for a page give a date and get the number of views.
 
-    ![Example startpage](src/images/Screenshot 2021-12-17 at 10.53.43.png)
+![Example startpage](src/images/Screenshot 2021-12-17 at 10.53.43.png)
 
 ## HOW TO USE
 
--- src/js/index.js
+++ src/js/index.js
@@ -10,53 +10,88 @@ const bodyRef = document.querySelector("body");
 const headerRef = document.querySelector("header");
 const infoRef = document.querySelector(".info");
 const chartRef = document.querySelector("#myChart").getContext("2d");
+const btnBar = document.querySelector(".bar");
+const btnPie = document.querySelector(".pie");
+const btnLine = document.querySelector(".line");
+const btnPolar = document.querySelector(".polar");
 
 Form(headerRef);
 InfoCard(infoRef);
 // ChartCard(chartRef);
+const data = {
+  labels: [],
+  datasets: [
+    {
+      label: "Amount of views per day",
+      data: [],
+      backgroundColor: [
+        "rgba(255, 99, 132)",
+        "rgba(54, 162, 235)",
+        "rgba(255, 206, 86)",
+        "rgba(75, 192, 192)",
+        "rgba(153, 102, 255)",
+        "rgba(255, 159, 64)",
+      ],
 
-let myChart = new Chart(chartRef, {
-  type: "pie",
-  data: {
-    labels: [],
-    datasets: [
-      {
-        label: "Amount of views a day",
-        data: [],
-        backgroundColor: [
-          "rgba(255, 99, 132)",
-          "rgba(54, 162, 235)",
-          "rgba(255, 206, 86)",
-          "rgba(75, 192, 192)",
-          "rgba(153, 102, 255)",
-          "rgba(255, 159, 64)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132)",
-          "rgba(54, 162, 235)",
-          "rgba(255, 206, 86)",
-          "rgba(75, 192, 192)",
-          "rgba(153, 102, 255)",
-          "rgba(255, 159, 64)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    legend: {
-      display: false,
+      hoverOffset: 4,
     },
+  ],
+};
+
+const configChartBar = {
+  type: "bar",
+  data,
+  options: {
     scales: {
+      legend: {
+        display: false,
+      },
       y: {
         beginAtZero: true,
       },
     },
   },
-});
+};
+const configChartPie = {
+  type: "pie",
+  data,
+};
+const configChartLine = {
+  type: "line",
+  data,
+};
+const configChartPolar = {
+  type: "polarArea",
+  data,
+};
+const initChartBar = () => {
+  myChart = new Chart(document.querySelector("#myChart"), configChartBar);
+};
+const initChartPie = () => {
+  myChart = new Chart(
+    document.querySelector("#myChart").getContext("2d"),
+    configChartPie
+  );
+};
+const initChartLine = () => {
+  myChart = new Chart(
+    document.querySelector("#myChart").getContext("2d"),
+    configChartLine
+  );
+};
+const initChartPolar = () => {
+  myChart = new Chart(
+    document.querySelector("#myChart").getContext("2d"),
+    configChartPolar
+  );
+};
+
+let myChart = new Chart(
+  document.querySelector("#myChart").getContext("2d"),
+  configChartBar
+);
 
 const render = () => {
-  myChart.data.labels = [];
   const timestamps = store.getState().data.map((obj) => obj.timestamp);
   const amountViews = store.getState().data.map((obj) => obj.views);
   myChart.data.labels.splice(0, myChart.data.labels.length, ...timestamps);
@@ -71,3 +106,47 @@ const render = () => {
 
 render();
 store.subscribe(render);
+
+btnBar.onclick = () => {
+  myChart.destroy();
+  initChartBar();
+  console.log("test");
+  myChart.config.type = "bar";
+  document
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
+  btnBar.classList.toggle("active");
+
+  myChart.update();
+};
+
+btnPie.onclick = () => {
+  myChart.destroy();
+  initChartPie();
+  console.log("test");
+  document
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
+  btnPie.classList.toggle("active");
+  myChart.update();
+};
+btnLine.onclick = (e) => {
+  e.preventDefault();
+  myChart.destroy();
+  initChartLine();
+  document
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
+  btnLine.classList.toggle("active");
+  myChart.update();
+};
+btnPolar.onclick = (e) => {
+  e.preventDefault();
+  myChart.destroy();
+  initChartPolar();
+  document
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
+  btnPolar.classList.toggle("active");
+  myChart.update();
+};
